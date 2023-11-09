fb9580550796175902032931c7ec521ec403ce05
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-11-09 19:25:47 +0700

    add request validation

--- internal/data/dto_passanger/passanger.go
+++ internal/data/dto_passanger/passanger.go
@@ -1,20 +1,20 @@
 package dto_passanger
 
 type RequestRegister struct {
-	DateOfBirth string `json:"date_of_birth"`
-	FullName    string `json:"full_name"`
-	Gender      string `json:"gender"`
-	IDNumber    string `json:"id_number"`
-	IDType      string `json:"id_type"`
+	DateOfBirth string `json:"date_of_birth" validate:"required,datetime=2006-01-02"`
+	FullName    string `json:"full_name" validate:"required"`
+	Gender      string `json:"gender" validate:"required"`
+	IDNumber    string `json:"id_number" validate:"required,numeric,min=16,max=16"`
+	IDType      string `json:"id_type" validate:"required"`
 }
 
 type RequestUpdate struct {
-	DateOfBirth string `json:"date_of_birth"`
-	FullName    string `json:"full_name"`
-	Gender      string `json:"gender"`
-	ID          int64  `json:"id"`
-	IDNumber    string `json:"id_number"`
-	IDType      string `json:"id_type"`
+	DateOfBirth string `json:"date_of_birth" validate:"required,datetime=2006-01-02"`
+	FullName    string `json:"full_name" validate:"required"`
+	Gender      string `json:"gender" validate:"required"`
+	ID          int64  `json:"id" validate:"required,numeric"`
+	IDNumber    string `json:"id_number" validate:"required,numeric,min=16,max=16"`
+	IDType      string `json:"id_type" validate:"required"`
 }
 
 type ResponseRegistered struct {
