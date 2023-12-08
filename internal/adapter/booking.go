f301e3a6adbc1af96c4282e05cd4d8d9de01b619
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-12-08 07:41:15 +0700

    feature: synchronus request

--- internal/adapter/booking.go
+++ internal/adapter/booking.go
@@ -1,39 +1,79 @@
 package adapter
 
 import (
+	"bytes"
 	"fmt"
 	"fww-wrapper/internal/data/dto"
 	"fww-wrapper/internal/data/dto_booking"
+	"net/http"
 
-	"github.com/ThreeDotsLabs/watermill"
-	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/goccy/go-json"
 	"github.com/mitchellh/mapstructure"
 )
 
-// Booking implements Adapter.
+// // Booking implements Adapter.
+// func (a *adapter) Booking(body *dto_booking.Request) (resp dto_booking.AsyncBookResponse, err error) {
+// 	json, err := json.Marshal(body)
+// 	if err != nil {
+// 		return resp, err
+// 	}
+
+// 	ID := watermill.NewUUID()
+
+// 	err = a.publisher.Publish("request_booking", message.NewMessage(
+// 		ID,
+// 		json,
+// 	))
+// 	if err != nil {
+// 		return resp, err
+// 	}
+
+// 	resp = dto_booking.AsyncBookResponse{
+// 		BookingIDCode: ID,
+// 	}
+
+// 	return resp, nil
+
+// }
+
 func (a *adapter) Booking(body *dto_booking.Request) (resp dto_booking.AsyncBookResponse, err error) {
-	json, err := json.Marshal(body)
+	url := fmt.Sprintf("http://%s:%s/api/private/v1/booking", a.cfg.Host, a.cfg.Port)
+
+	jsonBody, err := json.MarshalIndent(body, "", "  ")
 	if err != nil {
 		return resp, err
 	}
 
-	ID := watermill.NewUUID()
-
-	err = a.publisher.Publish("request_booking", message.NewMessage(
-		ID,
-		json,
-	))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
 	if err != nil {
 		return resp, err
 	}
 
-	resp = dto_booking.AsyncBookResponse{
-		BookingIDCode: ID,
+	req.Header.Set("Content-Type", "application/json")
+
+	response, err := a.client.Do(req)
+	if err != nil {
+		return resp, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return resp, err
+	}
+
+	var responseBase dto.BaseResponse
+
+	dec := json.NewDecoder(response.Body)
+	if err = dec.Decode(&responseBase); err != nil {
+		return
+	}
+
+	if err = mapstructure.Decode(responseBase.Data, &resp); err != nil {
+		return dto_booking.AsyncBookResponse{}, err
 	}
 
 	return resp, nil
-
 }
 
 // GetDetailBooking implements Adapter.
